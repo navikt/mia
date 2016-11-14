edf8f2dcbf7d465cdf8ab0a82766e07c7cc00f38
Hageland, Steffen <steffen.hageland@nav.no>
2016-11-14 11:52:50 +0100

    Initial commit
    Setter opp en bare prosjekt med serving av enkel index-fil

---
+++ README.md
@@ -0,0 +1,4 @@
+# Brukerflate
+
+NB. husk å ha Poll SCM i jenkinskonfig
+Litt mer tekst i readme!
---
+++ web/src/test/java/no/nav/fo/StartJetty.java
@@ -0,0 +1,19 @@
+package no.nav.fo;
+
+import no.nav.sbl.dialogarena.common.jetty.Jetty;
+
+import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
+import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.*;
+
+public class StartJetty {
+    private static final int PORT = 8486;
+
+    public static void main(String[] args) throws Exception {
+        Jetty jetty = usingWar()
+                .at("/brukerflate")
+                .loadProperties("/test.properties")
+                .port(PORT)
+                .buildJetty();
+        jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
+    }
+}
