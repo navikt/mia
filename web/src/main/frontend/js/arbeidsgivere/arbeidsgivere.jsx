d123870d0f75c5ef2632133a729fa0ee0dd42d95
Hageland, Steffen <steffen.hageland@nav.no>
2016-11-17 09:35:11 +0100

    Legger til manglende semikolon

--- web/src/main/frontend/js/arbeidsgivere/arbeidsgivere.jsx
+++ web/src/main/frontend/js/arbeidsgivere/arbeidsgivere.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import IkkeFerdigPanel from "../felles/ikkeferdig/ikke-ferdig-panel"
+import IkkeFerdigPanel from "../felles/ikkeferdig/ikke-ferdig-panel";
 
 const Arbeidsgivere = () => (
     <IkkeFerdigPanel />
