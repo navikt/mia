0f3149b1432180ac8e4c3b1717b145cd3fd39638
Hageland, Steffen <steffen.hageland@nav.no>
2016-11-23 08:23:11 +0100

    Legger til eksempelkart på siden

--- web/src/main/frontend/js/ledigestillinger/oversikt/ledigestillinger-oversikt-kart.jsx
+++ web/src/main/frontend/js/ledigestillinger/oversikt/ledigestillinger-oversikt-kart.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import IkkeFerdigPanel from "../../felles/ikkeferdig/ikke-ferdig-panel";
+import ISvg from "react-inlinesvg";
+import {defineMessages, injectIntl, FormattedMessage} from 'react-intl';
+
+const meldinger = defineMessages({
+    kartplaceholder: {
+        id: 'ledigestillinger.oversikt.kartplaceholder',
+        defaultMessage: 'Kart over {fylke}-fylke. Det er {stillinger} ledige stillinger.'
+    }
+});
 
 const Oversiktskart = () => {
     return (
         <div>
-            <h2>Oversiktskart</h2>
-            <IkkeFerdigPanel />
+            <h2 className="typo-innholdstittel blokk-s">Nordland, ledige stillinger totalt: 612</h2>
+            <div className="oversikt-kart">
+                <ISvg src="./img/fylker/nordland.svg">
+                    <FormattedMessage {...meldinger.kartplaceholder} fylke="nordland" stillinger="682"/>
+                </ISvg>
+            </div>
         </div>
     );
 };
 
-export default Oversiktskart;
+export default injectIntl(Oversiktskart);
