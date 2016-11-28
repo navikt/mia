3443f9999dfff247b263eed05a7ef21c7bdf07af
Oscar Eckbo <oscar.eckbo@nav.no>
2016-11-28 10:43:24 +0100

    PKULV-1934 Bruker react-intl for tekster. Fjerner bruk av col-lg-4.

--- web/src/main/frontend/js/ledigestillinger/stillinger/ledigestillinger-stillinger-visning.jsx
+++ web/src/main/frontend/js/ledigestillinger/stillinger/ledigestillinger-stillinger-visning.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 import Stilling from './ledigestillinger-stillinger-stilling';
+import {defineMessages, FormattedMessage} from 'react-intl';
+
+export const meldinger = defineMessages({
+    stilling: {
+        id: 'ledigestillinger.stillinger.stilling',
+        defaultMessage: 'Stilling'
+    },
+    arbeidsgiver: {
+        id: 'ledigestillinger.stillinger.arbeidsgiver',
+        defaultMessage: 'Arbeidsgiver'
+    },
+    soknadsfrist: {
+        id: 'ledigestillinger.stillinger.soknadsfrist',
+        defaultMessage: 'Søknadsfrist'
+    },
+    ingenstillinger: {
+        id: 'ledigestillinger.stillinger.ingenstillinger',
+        defaultMessage: 'Ingen ledige stillinger'
+    }
+});
+
 
 export const Stillingsvisning = (props) => {
     const mockstillinger = [{id: 13, stilling: "Mekaniker", arbeidsgiver: "Rett Bemanning", soknadsfrist: "29.10.2016", link: "www.finn.no/jobb/14"},
@@ -10,14 +31,14 @@ export const Stillingsvisning = (props) => {
             <h1>{props.valgteyrkesgrupper}</h1>
             <div className="panel panel-fremhevet">
                 <div className="row">
-                    <span className="col-lg-4 col-sm-4 typo-etikett-stor">Stilling</span>
-                    <span className="col-lg-4 col-sm-4 typo-etikett-stor">Arbeidsgiver</span>
-                    <span className="col-lg-4 col-sm-4 typo-etikett-stor">Søknadsfrist</span>
+                    <span className="col-sm-4 typo-etikett-stor"><FormattedMessage {...meldinger.stilling} /></span>
+                    <span className="col-sm-4 typo-etikett-stor"><FormattedMessage {...meldinger.arbeidsgiver} /></span>
+                    <span className="col-sm-4 typo-etikett-stor"><FormattedMessage {...meldinger.soknadsfrist} /></span>
                 </div>
-                <ul className="ustilet">
+                <ul className="ustilet list-with-hover">
                     {mockstillinger.length > 0 ? mockstillinger.map(stilling => {
                         return <Stilling stilling={stilling} key={stilling.id}/>;
-                    }) : <li className="row">Ingen ledige stillinger</li>}
+                    }) : <li className="row"><FormattedMessage {...meldinger.ingenstillinger} /></li>}
                 </ul>
             </div>
         </div>
