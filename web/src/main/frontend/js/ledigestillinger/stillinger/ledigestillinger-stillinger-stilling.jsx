59c979614b9b20f55f7540efd088e03cfc023bbe
Oscar Eckbo <oscar.eckbo@nav.no>
2016-11-28 10:42:32 +0100

    PKULV-1934 Bruker Link-komponent i steden for a-tag. Fjerner col-lg-4.

--- web/src/main/frontend/js/ledigestillinger/stillinger/ledigestillinger-stillinger-stilling.jsx
+++ web/src/main/frontend/js/ledigestillinger/stillinger/ledigestillinger-stillinger-stilling.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export const Stilling = (props) => {
     return (
-            <li className="row">
-                <a href={props.stilling.link}>
-                <span className="col-lg-4 col-sm-4">{props.stilling.stilling}</span><span className="col-lg-4 col-sm-4">{props.stilling.arbeidsgiver}</span><span className="col-lg-4 col-sm-4">{props.stilling.soknadsfrist}</span>
-                </a>
-            </li>
+        <li className="row">
+            <Link to={props.stilling.link}>
+                <span className="col-sm-4">{props.stilling.stilling}</span><span className="col-sm-4">{props.stilling.arbeidsgiver}</span><span className="col-sm-4">{props.stilling.soknadsfrist}</span>
+            </Link>
+        </li>
     );
 };
 
