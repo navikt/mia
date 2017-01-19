95ca94f3d58558a49363e328a267307bafc9c7c5
Oscar Eckbo <oscar.eckbo@nav.no>
2017-01-19 17:26:36 +0100

    Revert "Revert "Merge pull request #35 in FO/mia from feature/PKULV-1914_ledighetsdata to master""
    This reverts commit 50183fd725a4a991419bf90a90c8f93e7dfb22ea.

--- web/src/main/frontend/js/ledigestillinger/oversikt/ledigestillinger-oversikt-omradetabell.jsx
+++ web/src/main/frontend/js/ledigestillinger/oversikt/ledigestillinger-oversikt-omradetabell.jsx
@@ -3,13 +3,14 @@ import {getValgteKommunerForFylke, compareOmrader} from './ledigestillinger-over
 import KommuneTabell from './ledigestillinger-oversikt-kommunetabell';
 
 
-export const OmradeTabell = ({ valgteFylker, valgteKommuner, omrader, stillinger }) => {
+export const OmradeTabell = ({ valgteFylker, valgteKommuner, omrader, stillinger, ledighet }) => {
     const tabell = valgteFylker
         .sort(compareOmrader)
         .map(fylke => <KommuneTabell
             key={fylke.id}
             fylke={fylke}
             kommuner={getValgteKommunerForFylke(fylke.id, omrader, valgteKommuner)}
+            ledighet={ledighet}
             stillinger={stillinger}
         />);
 
