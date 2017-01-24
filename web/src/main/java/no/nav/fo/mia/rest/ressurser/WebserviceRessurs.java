be552b0733dabb108078861dc039539fdc014cfd
Ingrid Guren <ingrid.guren@nav.no>
2017-01-24 15:31:20 +0100

    Indekserer solr basert på SolrInputDocument og rest-kall.

--- web/src/main/java/no/nav/fo/mia/rest/ressurser/WebserviceRessurs.java
+++ web/src/main/java/no/nav/fo/mia/rest/ressurser/WebserviceRessurs.java
@@ -1,16 +1,37 @@
 package no.nav.fo.mia.rest.ressurser;
 
+import no.nav.fo.transformers.IndekserSolr;
 import no.nav.metrics.aspects.Timed;
 import org.springframework.stereotype.Controller;
 
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Controller
-@Path("/ledighet")
+@Path("/indeksersolr")
 @Produces(APPLICATION_JSON)
 @Timed
 public class WebserviceRessurs {
+
+    @Inject
+    IndekserSolr indekserSolr;
+
+    @GET
+    @Path("/arbeidsledigecore")
+    public String indekserMiASolrArbeidsledighet() {
+        indekserSolr.lesOgSkrivArbeidsledige();
+        return "hei";
+    }
+
+    @GET
+    @Path("/ledigestillingercore")
+    public String indekserMiASolrLedigeStillinger() {
+        indekserSolr.lesOgSkrivLedigeStillinger();
+        return "hei";
+    }
+
 }
--- web/src/test/java/no/nav/fo/StartJetty.java
+++ web/src/test/java/no/nav/fo/StartJetty.java
@@ -1,7 +1,5 @@
 package no.nav.fo;
 
-import no.nav.fo.endpoints.HentDataEndpoint;
-import no.nav.fo.transformers.CSVTransformer;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
@@ -17,12 +15,6 @@ public class StartJetty {
                 .overrideWebXml()
                 .port(PORT)
                 .buildJetty();
-
-
-        new CSVTransformer().lesArbeidsledighetCSV();
-        new CSVTransformer().lesLedigeStillingerCSV();
-
-        HentDataEndpoint.indekserSOLRIndex();
 
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
 
