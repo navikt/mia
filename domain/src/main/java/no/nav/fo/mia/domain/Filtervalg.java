f9898676cea5517ae806947f0c8b0648a3fdd377
Hageland, Steffen <steffen.hageland@nav.no>
2017-03-06 13:57:02 +0100

    fjerner flere unused imports

--- domain/src/main/java/no/nav/fo/mia/domain/Filtervalg.java
+++ domain/src/main/java/no/nav/fo/mia/domain/Filtervalg.java
@@ -1,9 +1,7 @@
 package no.nav.fo.mia.domain;
 
 import javax.ws.rs.QueryParam;
-import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Filtervalg {
     @QueryParam("yrkesomrade")
