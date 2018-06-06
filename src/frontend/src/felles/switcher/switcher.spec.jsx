daf23f11b1b150d0e6443f327e4719981695db57
Steffen Hageland <steffen.hageland@bekk.no>
2018-06-06 14:04:14 +0200

    FO-1064 Fikser resterende tester i frontend

--- src/frontend/src/felles/switcher/switcher.spec.jsx
+++ src/frontend/src/felles/switcher/switcher.spec.jsx
@@ -1,6 +1,5 @@
-import {expect, React} from '../../../test/test-helper';
+import React from 'react';
 import {shallow} from 'enzyme';
-import sinon from 'sinon';
 import {Switcher} from './switcher.jsx';
 
 describe('HovedmenyVisning', () => {
@@ -24,14 +23,14 @@ describe('HovedmenyVisning', () => {
         let props = lagProps();
 
         const element = shallow(<Switcher {...props} />);
-        expect(element.find('SwitcherKnapp').length).to.eql(3);
+        expect(element.find('SwitcherKnapp').length).toEqual(3);
     });
 
     it('viser første element by default', function () {
         let props = lagProps();
 
         const element = shallow(<Switcher {...props} />);
-        expect(element.find('#element1').length).to.eql(1);
+        expect(element.find('#element1').length).toEqual(1);
     });
 
     it('viser riktig valgt element', function () {
@@ -39,11 +38,11 @@ describe('HovedmenyVisning', () => {
         props.switchere = {'123': 2};
 
         const element = shallow(<Switcher {...props} />);
-        expect(element.find('#element3').length).to.eql(1);
+        expect(element.find('#element3').length).toEqual(1);
     });
 
     it('kan velge element som skal vises ved å klikke på knapp', function () {
-        let settValg = sinon.spy();
+        let settValg = jest.fn();
 
         let props = lagProps();
         props.settValg = settValg;
@@ -51,7 +50,7 @@ describe('HovedmenyVisning', () => {
         const element = shallow(<Switcher {...props} />);
         element.find('SwitcherKnapp').at(2).simulate('click');
 
-        expect(settValg).to.have.been.calledWith('123', 2);
+        expect(settValg).toHaveBeenCalledWith('123', 2);
     });
 
 });
