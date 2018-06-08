7d75768dc7502c25678d555e0f873b024a3d77af
Håkon Planke Holm <hakon.planke.holm@nav.no>
2018-06-08 12:04:57 +0200

    [FO-1064] la til not enforced

---
+++ src/main/kotlin/no/nav/fo/mia/config/ElasticConfig.kt
@@ -0,0 +1,45 @@
+package no.nav.fo.mia.config
+
+import no.nav.fo.mia.util.getRequiredProperty
+import org.apache.http.HttpHost
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.client.CredentialsProvider
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
+import org.elasticsearch.client.RestClient
+import org.elasticsearch.client.RestClientBuilder
+import org.elasticsearch.client.RestHighLevelClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Configuration
+@Profile("!mock")
+open class EsConfig {
+    @Bean
+    open fun elasticClient(): RestHighLevelClient =
+            getClientForUri(getRequiredProperty("MIASOLR_ELASTIC_URL"))
+
+    private fun getClientForUri(uri: String): RestHighLevelClient =
+            RestHighLevelClient(
+                    RestClient
+                            .builder(HttpHost(uri))
+                            .setHttpClientConfigCallback(HttpClientConfigCallback())
+            )
+}
+
+class HttpClientConfigCallback : RestClientBuilder.HttpClientConfigCallback {
+    override fun customizeHttpClient(httpAsyncClientBuilder: HttpAsyncClientBuilder): HttpAsyncClientBuilder =
+            httpAsyncClientBuilder.setDefaultCredentialsProvider(createCredentialsProvider())
+
+    private fun createCredentialsProvider(): BasicCredentialsProvider {
+        val credentialsProvider = BasicCredentialsProvider()
+        val credentials = UsernamePasswordCredentials(
+                getRequiredProperty("MIA_ELASTIC_USER"),
+                getRequiredProperty("MIA_ELASTIC_PSW")
+        )
+        credentialsProvider.setCredentials(AuthScope.ANY, credentials)
+        return credentialsProvider
+    }
+}
--- src/main/kotlin/no/nav/fo/mia/controllers/InternalController.kt
+++ src/main/kotlin/no/nav/fo/mia/controllers/InternalController.kt
@@ -1,16 +1,26 @@
 package no.nav.fo.mia.controllers
 
+import no.nav.fo.mia.provider.ElasticIndexProvider
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.inject.Inject
 
 @RestController
 @RequestMapping("/internal", produces = [(MediaType.TEXT_PLAIN_VALUE)])
-class InternalController {
+class InternalController @Inject
+constructor(val es: ElasticIndexProvider) {
     @GetMapping("/isAlive")
     fun isAlive() = "Application is UP"
 
     @GetMapping("/isReady")
     fun isReady() = "Application is READY"
+
+    @GetMapping("/createIndexes")
+    fun createIndexes(): String {
+        es.createArbeidsledigeIndex()
+        es.createStillingerIndex()
+        return es.getAll()
+    }
 }
