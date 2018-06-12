f910db0d48735a285e383267f2b416f4d2e8c6fe
hakonph <hakon.planke.holm@nav.no>
2018-06-12 15:36:03 +0200

    [FO-1064] merer resurser

--- src/main/kotlin/no/nav/fo/mia/controllers/InternalController.kt
+++ src/main/kotlin/no/nav/fo/mia/controllers/InternalController.kt
@@ -3,10 +3,12 @@ package no.nav.fo.mia.controllers
 import no.nav.fo.mia.service.IndeksererService
 import no.nav.fo.mia.util.arbeidsledigeIndex
 import no.nav.fo.mia.util.stillingerIndex
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import javax.inject.Inject
 import org.springframework.web.multipart.MultipartFile
+import kotlin.concurrent.thread
 
 @RestController
 @RequestMapping("/internal", produces = [(MediaType.TEXT_PLAIN_VALUE)])
@@ -14,6 +16,8 @@ class InternalController @Inject
 constructor(
         val service: IndeksererService
 ) {
+    private val LOGGER = LoggerFactory.getLogger(InternalController::class.java)
+
     @GetMapping("/isAlive")
     fun isAlive() = "Application is UP"
 
@@ -25,14 +29,17 @@ constructor(
 
     @PostMapping("/arbeidsledigecore")
     fun arbeidlsedigeIndex(@RequestParam("file")  file: MultipartFile): String {
-        println("oppdaterer: $arbeidsledigeIndex")
+        LOGGER.info(arbeidsledigeIndex)
         return service.recreatIndex(file.inputStream, arbeidsledigeIndex)
     }
 
     @PostMapping("/ledigestillingercore")
     fun stilingerIndex(@RequestParam("file")  file: MultipartFile): String {
-        println("oppdaterer: $arbeidsledigeIndex")
-        return service.recreatIndex(file.inputStream, stillingerIndex)
+        LOGGER.info(stillingerIndex)
+        thread {
+            service.recreatIndex(file.inputStream, stillingerIndex)
+        }
+        return ":)"
     }
 
 }
