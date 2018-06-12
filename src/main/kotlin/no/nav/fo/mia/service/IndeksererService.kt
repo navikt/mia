dd0d13547fb14f351aa0ec6d7438504812281c21
hakonph <hakon.planke.holm@nav.no>
2018-06-12 15:23:39 +0200

    [FO-1064] lagt til litt loggin

--- src/main/kotlin/no/nav/fo/mia/service/IndeksererService.kt
+++ src/main/kotlin/no/nav/fo/mia/service/IndeksererService.kt
@@ -6,6 +6,7 @@ import no.nav.fo.mia.util.indexMap
 import org.elasticsearch.action.bulk.BulkRequest
 import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.common.inject.Inject
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.io.InputStream
 import java.util.*
@@ -17,13 +18,17 @@ constructor(
         private val bransjeMappingService: SolrBransjeMappingService,
         private val elastic: ElasticIndexProvider
 ) {
+    private val LOGGER = LoggerFactory.getLogger(IndeksererService::class.java)
     fun recreatIndex(stream: InputStream, index: String): String{
         val bulk = BulkRequest()
         val headers = indexMap[index]!!
 
+        LOGGER.info("rteading inputdata for reindex")
         val allLines = stream
                 .reader()
                 .readLines()
+
+        LOGGER.info("remapping data")
 
         val requests = allLines
                 .subList(1, allLines.size) //fjerner header linjen
@@ -37,10 +42,14 @@ constructor(
 
         bulk.add(requests)
 
+
         val time = measureTimeMillis {    //TODO fiks denne til å vere nedetidsfri og tryggere (17 sec på mac med stor fil)
+            LOGGER.info("Ddeleting and creting index")
             elastic.recreateIndex(index)
+            LOGGER.info("indexing")
             elastic.index(bulk)
         }
+        LOGGER.info("Indexing took $time")
 
         return ":) $time"
     }
