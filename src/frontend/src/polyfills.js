e29fced0a57b41382f13978344cdb7cc14474903
Steffen Hageland <steffen.hageland@nav.no>
2018-06-21 11:59:58 +0200

    FO-1188 Legger til flere polyfills

--- src/frontend/src/polyfills.js
+++ src/frontend/src/polyfills.js
@@ -1,4 +1,7 @@
 import 'core-js/fn/array/includes';
+import 'core-js/fn/array/find';
+import 'core-js/fn/string/ends-with';
+import 'core-js/fn/string/starts-with';
 import 'core-js/fn/string/includes';
 import 'core-js/fn/object/entries';
 import 'whatwg-fetch';
