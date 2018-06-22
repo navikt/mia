1b8be20147953ce3efd8e3e945f8f05a30893d15
Steffen Hageland <steffen.hageland@bekk.no>
2018-06-22 12:36:35 +0200

    Revert "KAIZEN-0 Fjerner cache da det ikke er i bruk"
    This reverts commit 20f385a0728b0b9d5936168a040bdd6a937528d1.

--- src/main/kotlin/no/nav/fo/mia/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/fo/mia/config/ApplicationConfig.kt
@@ -4,7 +4,10 @@ import no.nav.fo.mia.util.CorsFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.jcache.JCacheCacheManager
+import javax.cache.Caching
 
 
 @Configuration
@@ -16,5 +19,15 @@ open class ApplicationConfig {
         frb.filter = CorsFilter()
         frb.urlPatterns = arrayListOf("/*")
         return frb
+    }
+
+    @Bean
+    open fun cacheManager(): CacheManager {
+        val cachingProvider = Caching.getCachingProvider()
+        val cacheManager = cachingProvider.getCacheManager(
+                javaClass.getResource("/cache.xml").toURI(),
+                cachingProvider.defaultClassLoader
+        )
+        return JCacheCacheManager(cacheManager)
     }
 }
