d74588aef7417c8cbf278e71477e47b2dd763aaf
Izis Frøyhov <izis.froyhov@nav.no>
2018-08-08 09:11:31 +0200

    FO-1385: Work around for edge browser

--- src/frontend/integrationtest/commands/oversikt-section.js
+++ src/frontend/integrationtest/commands/oversikt-section.js
@@ -1,8 +1,10 @@
 
 module.exports = {
     visTabell() {
+        this.moveToElement('@knappTabell', undefined, undefined);
         this.waitForElementVisible('@knappTabell', this.api.globals.timeout);
         this.click('@knappTabell');
+        this.moveToElement('@tabell', undefined, undefined);
         this.waitForElementVisible('@tabell', this.api.globals.timeout);
         return this;
     },
