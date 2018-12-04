3b08b954b4a5a6495b47d794860deec2ab7d1caf
hakonph <hakon.planke.holm@nav.no>
2018-12-04 14:27:54 +0100

    KAIZEN-0 redirekter tjenester.nav.no til mia.nav.no

---
+++ src/main/kotlin/no/nav/fo/mia/config/RedirectFilter.kt
@@ -0,0 +1,23 @@
+package no.nav.fo.mia.config
+
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.*
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+val tjensteUrl = """\Ahttps://tjenester(-q[0-9])?.nav.no/.*""".toRegex()
+
+@Component
+open class RedirectFilter : OncePerRequestFilter() {
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+        if(!request.isSecure || tjensteUrl.matches(request.requestURL)) {
+            response.sendRedirect("https://mia.nav.no/")
+        } else {
+            chain.doFilter(request, response)
+        }
+    }
+}
+
+
