d9b410eebc3d62ae005cc16d325f3ae6b0eab8f5
hakonph <hakon.planke.holm@nav.no>
2018-12-04 18:12:45 +0100

    KAIZEN-0  logging

--- src/main/kotlin/no/nav/fo/mia/config/RedirectFilter.kt
+++ src/main/kotlin/no/nav/fo/mia/config/RedirectFilter.kt
@@ -1,5 +1,6 @@
 package no.nav.fo.mia.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.*
@@ -8,13 +9,19 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 val tjensteUrl = """\Ahttps://tjenester(-q[0-9])?.nav.no/.*""".toRegex()
+val log = LoggerFactory.getLogger(RedirectFilter::class.java)
 
 @Component
 open class RedirectFilter : OncePerRequestFilter() {
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+
+        log.info(request.requestURI)
+
         if( tjensteUrl.matches(request.requestURL)) {
+            log.info("redirecter")
             response.sendRedirect(response.encodeRedirectURL("https://mia.nav.no/"))
         } else {
+            log.info("fortsetter")
             chain.doFilter(request, response)
         }
     }
