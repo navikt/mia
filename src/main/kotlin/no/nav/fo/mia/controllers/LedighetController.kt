1541861330bcec5e0a8699ebce6488026548f3ee
hakonph <hakon.planke.holm@nav.no>
2019-02-11 12:07:19 +0100

    FO-1822 fikser feil anotaskjon

--- src/main/kotlin/no/nav/fo/mia/controllers/LedighetController.kt
+++ src/main/kotlin/no/nav/fo/mia/controllers/LedighetController.kt
@@ -4,10 +4,10 @@ import no.nav.fo.mia.Filtervalg
 import no.nav.fo.mia.consumers.StatestikkConsumer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.HashMap
 import javax.inject.Inject
+import javax.ws.rs.BeanParam
 
 @RestController
 @RequestMapping("/rest/ledighet")
@@ -15,7 +15,7 @@ class LedighetController2 @Inject
 constructor(val statestikkService: StatestikkConsumer) {
 
     @GetMapping("/statistikk")
-    fun hentLedighetForSisteTrettenMaaneder(@RequestParam filtervalg: Filtervalg): Map<String, Map<String, Int?>> {
+    fun hentLedighetForSisteTrettenMaaneder(@BeanParam filtervalg: Filtervalg): Map<String, Map<String, Int?>> {
         val underkategorier = underkategorierFra(filtervalg)
         val komuner = kommunerFra(filtervalg)
         val statistikk = HashMap<String, Map<String, Int?>>()
